1ec199379a2907dddee4e9d8517e3bdd0b54e5cd
hareetd <hareet_dhillon@sfu.ca>
2020-10-29 08:47:25 -0700

    Update app/src/main/java/com/cmpt276/teal/parentingpro/data/AppDataKey.java

--- app/src/main/java/com/cmpt276/teal/parentingpro/data/AppDataKey.java
+++ app/src/main/java/com/cmpt276/teal/parentingpro/data/AppDataKey.java
@@ -9,6 +9,6 @@ public class AppDataKey
 {
     public static final String LAST_COIN_WIN = "last_coin_win";
 
-    public static final String COPIN_HISTORY = "coin_history";
+    public static final String COIN_HISTORY = "coin_history";
 
 }
