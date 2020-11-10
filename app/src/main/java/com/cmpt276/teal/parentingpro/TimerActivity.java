bb6407e685eb0f3d27cc5bc3341e240facf5f848
Kemo <ksonko@sfu.ca>
2020-11-09 22:31:51 -0800

    pushing tag final_1

--- app/src/main/java/com/cmpt276/teal/parentingpro/TimerActivity.java
+++ app/src/main/java/com/cmpt276/teal/parentingpro/TimerActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
