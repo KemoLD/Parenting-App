2471d52ca6b7a204050c952232b8970cc33da097
ksonko <kemo_sonko@sfu.ca>
2020-11-18 15:14:47 -0800

    Revert "added context"
    This reverts commit cb2d2017e5ee57b81774a4d0399b38389e640005

--- app/src/main/java/com/cmpt276/teal/parentingpro/model/Child.java
+++ app/src/main/java/com/cmpt276/teal/parentingpro/model/Child.java
@@ -1,17 +1,11 @@
 package com.cmpt276.teal.parentingpro.model;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-
 public class Child
 {
     private String name;
-    private Bitmap profile;
-    private final Context context ;
 
-    public Child(String name, Context context){
+    public Child(String name){
         this.name = name;
-        this.context = context;
     }
 
     public String getName(){
@@ -20,14 +14,6 @@ public class Child
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setProfile(Bitmap bitmap){
-        profile = bitmap;
-    }
-
-    public Bitmap getProfile(){
-        return profile;
     }
 
     public boolean equals(Child otherChild){
