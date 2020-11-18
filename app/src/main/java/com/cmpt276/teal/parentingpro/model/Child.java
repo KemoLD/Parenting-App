d2b0b6f748924f6569272e93a24555e32d67db10
Kemo <ksonko@sfu.ca>
2020-11-18 15:33:27 -0800

    fixed child item

--- app/src/main/java/com/cmpt276/teal/parentingpro/model/Child.java
+++ app/src/main/java/com/cmpt276/teal/parentingpro/model/Child.java
@@ -1,11 +1,30 @@
 package com.cmpt276.teal.parentingpro.model;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.cmpt276.teal.parentingpro.R;
+
 public class Child
 {
     private String name;
+    private Bitmap profile;
+    private Context context;
 
-    public Child(String name){
+    public Child(String name, Context context){
         this.name = name;
+        this.context = context;
+        profile = BitmapFactory.decodeResource(context.getResources(),
+                R.drawable.default_profile_pic);
+    }
+
+    public void setProfile(Bitmap bitmap){
+        profile = bitmap;
+    }
+
+    public Bitmap getProfile(){
+        return profile;
     }
 
     public String getName(){
