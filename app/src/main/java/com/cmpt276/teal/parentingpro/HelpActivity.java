012b6c77cea6fed74a4d85402979eb31f2edf517
Hareet Dhillon <hareet.dhillon@gmail.com>
2020-11-23 20:04:23 -0800

    Add colourful UI to help page

--- app/src/main/java/com/cmpt276/teal/parentingpro/HelpActivity.java
+++ app/src/main/java/com/cmpt276/teal/parentingpro/HelpActivity.java
@@ -1,18 +1,26 @@
 package com.cmpt276.teal.parentingpro;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
+        setUpCopyrightLinks();
     }
 
     @Override
@@ -21,5 +29,17 @@ public class HelpActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setUpCopyrightLinks() {
+        TextView textView = findViewById(R.id.text_view_copyright_body);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            textView.setText(Html.fromHtml(getString(R.string.copyright_body_text), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            textView.setText(Html.fromHtml(getString(R.string.copyright_body_text), Html.FROM_HTML_MODE_LEGACY));
+        }
+        Linkify.addLinks(textView, Linkify.ALL);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
